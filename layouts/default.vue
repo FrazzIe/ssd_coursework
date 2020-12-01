4628ebf87b95a7060e0b2367d57f2c8dcf179347
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-12-01 16:04:51 +0000

    add basic navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,49 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container fluid>
-        <nuxt />
-      </v-container>
-    </v-main>
-  </v-app>
+	<v-app>
+		<v-app-bar fixed app>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Title</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-toolbar-items>
+				<template v-if="isAuthenticated">
+					<v-btn v-if="canAccess('canCreateTicket')" elevation="0">OPEN TICKET</v-btn>
+				</template>
+				<template v-else>
+				</template>
+			</v-toolbar-items>
+		</v-app-bar>
+		<v-main>
+			<v-container fluid>
+				<nuxt />
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <style>
 </style>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
 	data: () => ({
 
-	})
+	}),
+	computed: {
+		...mapGetters(["isAuthenticated", "loggedInUser"]),
+	},
+	methods: {
+		canAccess(permission) {
+			const [err, result] = this.$permission.check(this.loggedInUser.scope, permission);
+
+			if (err) {
+				console.log(err);
+				return false;
+			}
+
+			return result;
+		}
+	}
 }
 </script>

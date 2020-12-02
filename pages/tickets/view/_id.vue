c38da438051d881afbfe340d7b805a08e0b9a3fa
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-12-02 19:23:44 +0000

    add base page for viewing a ticket

---
+++ pages/tickets/view/_id.vue
@@ -0,0 +1,53 @@
+<template>
+	<v-row justify="center" align="center">
+		<v-col sm="7" md="6" lg="5" xl="4">
+			<v-card class="elevation-12" tile>
+
+			</v-card> 
+		</v-col>
+		<snack-bar :snack-message.sync="snack.message" :snack-color="snack.color"></snack-bar>
+		<loader :message="loader.message" :show="loader.show"></loader>
+	</v-row>
+</template>
+
+<style>
+</style>
+
+<script>
+import { mapGetters } from "vuex";
+import snackBar from "@/components/snackBar";
+import loader from "@/components/loader";
+
+export default {
+	components: {
+		snackBar,
+		loader,
+	},
+	middleware: ["auth", "permissions"],
+    auth: true,
+    permissions: false,
+	data: () => ({
+		snack: {
+			message: "",
+			color: "",
+		},
+		loader: {
+			message: "",
+			show: false,
+		}
+	}),
+	computed: {
+		...mapGetters(["loggedInUser"]),
+	},
+	methods: {
+
+    },
+    validate({ params }) {
+        return !isNaN(+params.id);
+    },
+    async asyncData({ params, error }) {
+        const { ticket } = await this.$axios.$get(`/api/tickets/view/${+params.id}`);
+        return ticket;
+    }
+}
+</script>
